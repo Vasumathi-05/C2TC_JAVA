9d7aa897c1c669a89e69e76b78f72660b9f0d4b5
Admin <Admin@192.168.1.2>
2022-06-23 13:14:47 +0530

    ArraySearch

---
+++ SumofLengths of Array/src/sumoflengths.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sumoflengths {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String A=sc.next();
+	String B=sc.next();
+	System.out.println(A);
+	System.out.println(B);
+	System.out.println(A.length()+B.length());
+
+	}
+
+}
---
+++ oddeven/src/oddeven/odev.java
@@ -0,0 +1,21 @@
+package oddeven;
+
+import java.util.Scanner;
+
+public class odev {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter a number");
+		n=in.nextInt();
+		if(((n&1)==0))
+		{
+			System.out.println("given no is even");
+		}
+		else
+		{
+		System.out.println("given no is odd");
+	}
+	}
+}
