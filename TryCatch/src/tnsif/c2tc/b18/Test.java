f13bd31254c7120e9e1ce98daf578a7ae3f26451
Admin <Admin@192.168.1.4>
2022-07-01 12:58:27 +0530

    Trycatch

---
+++ TryCatch/src/tnsif/c2tc/b18/Test.java
@@ -0,0 +1,21 @@
+package tnsif.c2tc.b18;
+
+public class Test {
+
+	public static void main(String[] args) {
+		try {
+			int a[]= {1,2,3,4};
+			for(int i=1;i<=4;i++)
+			{
+				System.out.println("a["+i+"]="+a[i]);
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException");
+		}
+		catch(Exception e) {
+		System.out.println("error="+e);
+	}
+
+}
+}
