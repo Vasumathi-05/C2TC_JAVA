c1dbed6eb12f5acecea5a3b8c7ca4a5037d02d47
Venu <Venu@192.168.1.7>
2022-07-17 20:33:33 +0530

    ArrayDlt

---
+++ ArrayDlt/src/C2TC/B18/Array/array.java
@@ -0,0 +1,26 @@
+package C2TC.B18.Array;
+
+public class array {
+
+	public static void main(String[] args) {
+		int[] a= {10,20,30,40,50};
+		int delete=30;
+		for(int i=0;i<a.length;i++)
+		{
+			if(delete==a[i])
+			{
+				for(int j=i;j<a.length-1;j++)
+				{
+					a[j]=a[j+1];
+				}
+				break;
+			}
+		}
+		for(int i=0;i<a.length-1;i++)	
+		{
+			System.out.println(a[i]+" ");
+		}
+
+	}
+
+}
